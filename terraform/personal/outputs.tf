762efb161dbc9b2dd479a44e33cfc811a207ad6b
mzDAN <mh.ziaei.dan@gmail.com>
2025-04-16 23:00:59 +0330

    Also comment output

--- terraform/personal/outputs.tf
+++ terraform/personal/outputs.tf
@@ -1,11 +1,11 @@
-output "ipv4_addresses" {
-  value = proxmox_virtual_environment_vm.terraform001.ipv4_addresses[1][0]
-}
+# output "ipv4_addresses" {
+#   value = proxmox_virtual_environment_vm.terraform001.ipv4_addresses[1][0]
+# }
 
-output "hostname" {
-  value = proxmox_virtual_environment_vm.terraform001.name
-}
+# output "hostname" {
+#   value = proxmox_virtual_environment_vm.terraform001.name
+# }
 
-output "vm_id" {
-  value = proxmox_virtual_environment_vm.terraform001.vm_id
-}
+# output "vm_id" {
+#   value = proxmox_virtual_environment_vm.terraform001.vm_id
+# }
